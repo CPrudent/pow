7fe1c2d42d8dfee3ec9863917696eb0016f14867
Christophe Prudent <christophe.prudent@laposte.net>
2023-01-17 14:14:46 +0100

    #3 : create bashenv

---
+++ lib/bashenv.sh
@@ -0,0 +1,15 @@
+    #--------------------------------------------------------------------------
+    # synopsis
+    #--
+    # BASH environment
+
+# global config
+# remember that BASH can't export array!
+# https://unix.stackexchange.com/questions/393091/unable-to-use-an-array-as-environment-variable
+declare -A POW_CONF=(
+    [JAVA_HOME]=/usr/lib/jvm/default-java
+    [PG_DBNAME]=pow
+    [PG_VERSION]=15
+    [POSTGIS_VERSION]=3
+    [PG_PORT]=5432
+)
