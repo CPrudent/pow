4b1c8ea81303623f0fdd263df7bf3055bd156fbe
Christophe Prudent <christophe.prudent@laposte.net>
2023-02-22 16:34:14 +0100

    #25 : relocate & review restore (schema)

---
+++ bin/fr/db.objects.d/actions/purge_after_relocate.sql
@@ -0,0 +1,37 @@
+/***
+ * DB: deal w/ purge (after relocate)
+ */
+
+DO $PURGE$
+DECLARE
+    _schema_name VARCHAR;
+    _ntables INT;
+    _query VARCHAR;
+BEGIN
+    FOR _schema_name IN (
+        SELECT
+            *
+        FROM (
+            VALUES ('bal'), ('divers'), ('geopad'), ('ran')
+        ) AS t(schema)
+    )
+    LOOP
+        IF schema_exists(_schema_name) THEN
+            _query := CONCAT(
+                'SELECT COUNT(*) FROM pg_tables WHERE schemaname = '
+                , quote_ident(_schema_name)
+            );
+            EXECUTE _query USING _schema_name INTO _ntables;
+            IF _ntables = 0 THEN
+                _query := CONCAT(
+                    'DROP SCHEMA '
+                    , quote_ident(_schema_name)
+                    , ' CASCADE'
+                );
+                EXECUTE _query;
+            ELSE
+                RAISE NOTICE 'Ne peut supprimer le schéma(%), il existe encore #% table(s)', _schema_name, _ntables;
+            END IF;
+        END IF;
+    END LOOP;
+END $PURGE$;
