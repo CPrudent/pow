6a8b3d9d64de1f813f6e7ab6ef6ec46d2f7e459b
Christophe Prudent <christophe.prudent@laposte.net>
2024-09-01 09:20:56 +0200

    #75-no-uncommon: PG16

--- lib/bashenv.sh
+++ lib/bashenv.sh
@@ -9,7 +9,7 @@
 declare -A POW_CONF=(
     [JAVA_HOME]=/usr/lib/jvm/default-java
     [PG_DBNAME]=pow
-    [PG_VERSION]=15
+    [PG_VERSION]=16
     [POSTGIS_VERSION]=3
     [PG_HOST]=localhost
     [PG_PORT]=5432
