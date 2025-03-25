22f6404f12e62852cb8e406856a1f6738ac21bf6
Christophe Prudent <christophe.prudent@laposte.net>
2025-03-25 19:41:22 +0100

    #20 count errors

--- tests/test_libpg.sh
+++ tests/test_libpg.sh
@@ -15,7 +15,7 @@ declare -A env_libpg=(
 ) &&
 pow_argv \
     --args_n '
-        test:Préciser le test à réaliser
+        test:Préciser le test à réaliser (ALL pour tous)
     ' \
     --args_m '
         test
@@ -28,22 +28,22 @@ pow_argv \
     ' \
     --pow_argv env_libpg "$@" || exit $ERROR_CODE
 
-declare -a tests_libpg
-[ "${env_libpg[TEST]}" = ALL ] && tests_libpg=( "${TESTS[@]}" ) || tests_libpg[0]="${env_libpg[TEST]}"
+declare -a test_libpg
+[ "${env_libpg[TEST]}" = ALL ] && test_libpg=( "${TESTS[@]}" ) || test_libpg[0]="${env_libpg[TEST]}"
 declare -A result_libpg
 
 # tests
 set_log_echo no
-for ((_test=0; _test<${#tests_libpg[@]}; _test++)); do
+for ((_test=0; _test<${#test_libpg[@]}; _test++)); do
     _rc=1
-    case "${tests_libpg[$_test]}" in
+    case "${test_libpg[$_test]}" in
     EXECUTE_QUERY_RETURN)
         execute_query --name RETURN_VALUE --query 'SELECT 1+2' --return _value &&
         [ "$_value" -eq 3 ] &&
         _rc=0
         ;;
     EXECUTE_QUERY_OUTPUT)
-        _output=$POW_DIR_TMP/tests_libpg.output.tmp
+        _output=$POW_DIR_TMP/test_libpg.output.tmp
         execute_query --name OUTPUT_VALUE --query 'SELECT 1+2' --output $_output &&
         [ -s "$_output" ] &&
         _value=$(< $_output) &&
@@ -60,16 +60,30 @@ for ((_test=0; _test<${#tests_libpg[@]}; _test++)); do
         ;;
     TABLE_SEQUENCES)
         _seq=$(get_table_sequences --schema_name public --table_name io_history) &&
-        [ "$_seq " = io_history_id_seq ] &&
+        [ "$_seq" = io_history_id_seq ] &&
         _rc=0
         ;;
     esac
-    result_libpg+=(["${tests_libpg[$_test]}"]=$_rc)
+    [[ $_rc -ne 0 ]] && ((env_libpg[ERROR]++))
+    result_libpg+=(["${test_libpg[$_test]}"]=$_rc)
 done
 
 # results
-for ((_test=0; _test<${#tests_libpg[@]}; _test++)); do
-    printf "%s\t\t[%s]\n" "${tests_libpg[$_test]}" $( [[ ${result_libpg["${tests_libpg[$_test]}"]} -eq 0 ]] && echo OK || echo KO )
+_error=
+[[ ${env_libpg[ERROR]} -gt 0 ]] && _error="\navec ${env_libpg[ERROR]} erreur"
+[[ ${env_libpg[ERROR]} -gt 1 ]] && _error+=s
+_rc=$SUCCESS_CODE
+[ -n "$_error" ] && {
+    echo -e "$_error\n"
+    _rc=$ERROR_CODE
+}
+for ((_test=0; _test<${#test_libpg[@]}; _test++)); do
+    _to_print=$((40 - ${#test_libpg[$_test]}))
+    _spaces=$(printf ' %.0s' $(seq 1 $_to_print))
+    printf "%s%s[%s]\n" \
+        "${test_libpg[$_test]}" \
+        "$_spaces" \
+        $( [[ ${result_libpg["${test_libpg[$_test]}"]} -eq 0 ]] && echo OK || echo KO )
 done
 
-exit $SUCCESS_CODE
+exit $_rc
