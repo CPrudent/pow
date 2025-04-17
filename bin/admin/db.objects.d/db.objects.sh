91d64929247ff5226986d1c7d71bc41b0d084626
Christophe Prudent <christophe.prudent@laposte.net>
2025-04-17 20:42:24 +0200

    #27 db.objects.sh w/ pow_argv (0N), code review

--- bin/admin/db.objects.d/db.objects.sh
+++ bin/admin/db.objects.d/db.objects.sh
@@ -16,9 +16,9 @@ _schemas[0]=admin
 # NOTE: end with 'public' schema
 _schemas+=(public)
 
-# transform as pipe-delimited (to possible values 'schema_only' below)
-_schemas_join_pipe=${_schemas[@]}
-_schemas_join_pipe=${_schemas_join_pipe// /|}
+# transform as pipe-delimited (enable values of 'schema_only' below)
+_SCHEMAS_JOIN_PIPE=${_schemas[@]}
+_SCHEMAS_JOIN_PIPE=${_SCHEMAS_JOIN_PIPE// /|}
 
 # TODO add reset option to delete all tables
 # https://stackoverflow.com/questions/3327312/how-can-i-drop-all-the-tables-in-a-postgresql-database
@@ -30,7 +30,7 @@ pow_argv \
         relocate:Indicateur de changement de schéma (après restauration)
     ' \
 	--args_v "
-        schema_only:${_schemas_join_pipe};
+        schema_only:${_SCHEMAS_JOIN_PIPE};
         constant:no|yes;
         relocate:no|yes
     " \
@@ -45,7 +45,8 @@ pow_argv \
 
 [ -n "${POW_ARGV[SCHEMA_ONLY]}" ] && _schemas=(${POW_ARGV[SCHEMA_ONLY]})
 
-log_info "Mise à jour de la structure de la base de données"
+_error='' &&
+log_info "Mise à jour de la structure de la base de données" &&
 # superuser
 set_env --schema_name admin &&
 # need drop_all_functions_if_exists()
@@ -55,52 +56,56 @@ execute_query \
 # needed to avoid error "type geometry not exists"
 execute_query \
     --name PREPARE_EXTENSION_POSTGIS \
-    --query "$POW_DIR_BATCH/db.objects.d/actions/extension_postgis.sql" || exit $ERROR_CODE
+    --query "$POW_DIR_BATCH/db.objects.d/actions/extension_postgis.sql" &&
 for _schema in ${_schemas[@]}; do
     # begins w/ admin (core functions)
     # be careful, because relocation has to be run by superuser
-    if [ -f "$POW_DIR_BATCH/../${_schema}/db.objects.d/db.objects.sql" ] || ([ -f "$POW_DIR_BATCH/db.objects.d/actions/relocate.sql" ] && [ "${POW_ARGV[RELOCATE]}" = yes ]); then
-        log_info "Traitement schéma($_schema)" &&
-        {
-            {
-                if ([ -f "$POW_DIR_BATCH/../${_schema}/db.objects.d/actions/relocate.sql" ] && [ "${POW_ARGV[RELOCATE]}" = yes ]); then
-                    execute_query \
-                        --name RELOCATE \
-                        --query "$POW_DIR_BATCH/../${_schema}/db.objects.d/actions/relocate.sql"
-                fi
-            } &&
-            set_env --schema_name $_schema &&
-            {
-                if [ -f "$POW_DIR_BATCH/db.objects.d/db.objects.sql" ]; then
-                    execute_query \
-                        --name CREATE_OBJECTS \
-                        --query "$POW_DIR_BATCH/db.objects.d/db.objects.sql"
-                fi
-            } &&
-            {
-                if [ "${POW_ARGV[CONSTANT]}" = yes ] && [ -x "$POW_DIR_BATCH/constant.sh" ]; then
-                    $POW_DIR_BATCH/constant.sh
-                fi
-            } &&
-            set_env --schema_name admin &&
-            {
-                if ([ -f "$POW_DIR_BATCH/../${_schema}/db.objects.d/actions/relocate.sql" ] && [ "${POW_ARGV[RELOCATE]}" = yes ] && [ -f "$POW_DIR_BATCH/../${_schema}/db.objects.d/actions/purge_after_relocate.sql" ]); then
-                    execute_query \
-                        --name RELOCATE_PURGE \
-                        --query "$POW_DIR_BATCH/../${_schema}/db.objects.d/actions/purge_after_relocate.sql"
-                fi
-            }
-        } || {
-            log_error "Echec mise à jour de la structure de $_schema"
-            exit $ERROR_CODE
+    log_info "Traitement schéma($_schema)" &&
+    # relocate backup data (from BCAA)
+    {
+        [ "${POW_ARGV[RELOCATE]}" = no ] ||
+        [ ! -f "$POW_DIR_BATCH/../${_schema}/db.objects.d/actions/relocate.sql" ] ||
+        execute_query \
+            --name RELOCATE \
+            --query "$POW_DIR_BATCH/../${_schema}/db.objects.d/actions/relocate.sql"
+    } &&
+    set_env --schema_name $_schema &&
+    # objects of schema
+    {
+        [ ! -f "$POW_DIR_BATCH/db.objects.d/db.objects.sql" ] || {
+            execute_query \
+                --name CREATE_OBJECTS \
+                --query "$POW_DIR_BATCH/db.objects.d/db.objects.sql"
         }
-    fi
-done
+    } &&
+    # create constants (if exist)
+    {
+        [ "${POW_ARGV[CONSTANT]}" = no ] ||
+        [ ! -x "$POW_DIR_BATCH/constant.sh" ] ||
+        $POW_DIR_BATCH/constant.sh
+    } &&
+    set_env --schema_name admin &&
+    {
+        [ "${POW_ARGV[RELOCATE]}" = no ] ||
+        [ ! -f "$POW_DIR_BATCH/../${_schema}/db.objects.d/actions/relocate.sql" ] ||
+        [ ! -f "$POW_DIR_BATCH/../${_schema}/db.objects.d/actions/purge_after_relocate.sql" ] ||
+        execute_query \
+            --name PURGE_AFTER_RELOCATE \
+            --query "$POW_DIR_BATCH/../${_schema}/db.objects.d/actions/purge_after_relocate.sql"
+    } || {
+        _error "Echec mise à jour de la structure de $_schema"
+        break
+    }
+done &&
+[ -z "$_error" ] &&
 execute_query \
     --name 'PERMISSIONS' \
     --query "$POW_DIR_BATCH/db.objects.d/actions/grant.sql" &&
 execute_query \
     --name 'PURGE' \
-    --query "$POW_DIR_BATCH/db.objects.d/actions/purge.sql" || exit $ERROR_CODE
+    --query "$POW_DIR_BATCH/db.objects.d/actions/purge.sql" || {
+    [ -n "$_error" ] && log_error "$_error"
+    exit $ERROR_CODE
+}
 
 exit $SUCCESS_CODE
